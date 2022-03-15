7dd7aaac15fbb4419a67557f575a3459c2088ed5
JhonyWalker1 <jhonatanaliaga4@gmail.com>
2022-03-14 21:37:15 -0500

    ini2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// el primer paso para definir un componen es el nombre
+//El nombre de un componente siempre debe iniciar en mayuscula
+//ahora el componen basicamente es una funcion, por ende podemos crearlo
+// usando function o arrow function
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const PrimerComponente=()=>{
+  //esta funcion retorna una vista
+  //en react usamos return() para indicar que lo que esta dentro del parantesis sera el objeto que vamos a retornar
+  //algo importante es que react solo retorna un componente a la vez
+  // es decir solo retorna un div
+  return(
+    <div>
+      <h1>Hola mundo</h1>
+      <div>
+        <h4>Hola</h4>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+// luego de crear el componente debemos exportalo
+//Este archivo unicamente esta exportando este componente
+export default PrimerComponente;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+//para importar un componente debemos llamarlo por el nombre que fue exportado
+import PrimerComponente from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/para importar un componente debemos tratarlo como si fuera una etiqueta html*/}
+    <PrimerComponente />
   </React.StrictMode>,
   document.getElementById('root')
 );
