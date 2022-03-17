735d46ebaccd793d7ef9821ed7685cb9008b0ed4
JhonyWalker1 <jhonatanaliaga4@gmail.com>
2022-03-17 01:08:19 -0500

    pokemon en react

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-//para importar un componente debemos llamarlo por el nombre que fue exportado
-import PrimerComponente from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// Al importar el archivo index.css en index.js
+// esto esta haciendo un import global, es decir lo puedo desde cualquiervo del proyecto
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+// Para poder importar un componente debos llamarlo por el nombre que fue exportado
+// import PrimerComponente from "./App";
+// Por default el archivo que se exporta es index asi que no hace falta llamarlo
+import Home from "./pages/Home";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/para importar un componente debemos tratarlo como si fuera una etiqueta html*/}
-    <PrimerComponente />
+    {/* Para usar un componente debo tratarlo como si fuera una etiqueta HTML */}
+    {/* <PrimerComponente /> */}
+    <Home />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
